4c2827a33e6023ba50c2376b29348cf311218023
Vishesh Vikesh <imvk40@gmail.com>
2021-08-01 18:10:21 +0530

    initial commit

---
+++ src/main/java/CityRanchi/vj/App.java
@@ -0,0 +1,13 @@
+package CityRanchi.vj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
